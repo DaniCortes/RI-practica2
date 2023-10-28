aacae6f2c93b502df20a1de3b0fcaa738884346e
DaniCortes <danielcortes2001@gmail.com>
2023-10-28 18:02:36 +0200

    Second commit

--- src/main/java/org/example/Document.java
+++ src/main/java/org/example/Document.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map.Entry;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.CharArraySet;
@@ -20,6 +24,7 @@ import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.AutoDetectParser;
 import org.apache.tika.parser.ParseContext;
 import org.apache.tika.sax.BodyContentHandler;
+import org.jetbrains.annotations.NotNull;
 import org.xml.sax.SAXException;
 
 import java.io.*;
@@ -32,115 +37,186 @@ import java.util.stream.Stream;
 
 public class Document {
 
-    private final File file;
-    private String language;
-    private final BodyContentHandler textHandler;
-    private final Metadata metadata;
+  private final File file;
+  private String language;
+  private final BodyContentHandler textHandler;
+  private final Metadata metadata;
+  private final Map<String, Integer> languageTokens;
+  private List<Entry<String, Integer>> sortedLanguageTokens;
+  private final Map<String, Integer> standardTokens;
+  private List<Entry<String, Integer>> sortedStandardTokens;
 
-    public Document(File file) throws TikaException, IOException, SAXException {
-        this.file = file;
+  private final Map<String, Integer> whitespaceTokens;
+  private List<Entry<String, Integer>> sortedWhitespaceTokens;
 
-        AutoDetectParser parser = new AutoDetectParser();
-        textHandler = new BodyContentHandler(-1);
-        metadata = new Metadata();
-        ParseContext parseContext = new ParseContext();
 
-        //Esto lo he hecho porque necesito que el FileInputStream tenga soporte
-        // de mark/reset ya que si el InputStream que le paso al charsetDetector
-        // no tiene este soporte, da error.
-        InputStream is = new BufferedInputStream(new FileInputStream(file));
-        parser.parse(is, textHandler, metadata, parseContext);
-        retrieveLanguage();
+  public Document(File file) throws TikaException, IOException, SAXException {
+    this.file = file;
+
+    AutoDetectParser parser = new AutoDetectParser();
+    textHandler = new BodyContentHandler(-1);
+    metadata = new Metadata();
+    ParseContext parseContext = new ParseContext();
+
+    //Esto lo he hecho porque necesito que el FileInputStream tenga soporte
+    // de mark/reset ya que si el InputStream que le paso al charsetDetector
+    // no tiene este soporte, da error.
+    InputStream is = new BufferedInputStream(new FileInputStream(file));
+    parser.parse(is, textHandler, metadata, parseContext);
+    retrieveLanguage();
+    languageTokens = tokenizeDocument("language");
+    standardTokens = tokenizeDocument("standard");
+    whitespaceTokens = tokenizeDocument("");
+  }
+
+  private String identifyLanguage() {
+    LanguageDetector identifier = new OptimaizeLangDetector().loadModels();
+    LanguageResult language = identifier.detect(textHandler.toString());
+    return language.getLanguage();
+  }
+
+  private void retrieveLanguage() {
+    language = metadata.get("dc.language");
+
+    if (language == null) {
+      language = metadata.get("Content-Language");
+      if (language == null) {
+        language = identifyLanguage();
+      }
     }
+  }
 
-    private String identifyLanguage() {
-        LanguageDetector identifier = new OptimaizeLangDetector().loadModels();
-        LanguageResult language = identifier.detect(textHandler.toString());
-        return language.getLanguage();
+  private String getFilenameWithoutExtension() {
+    return FilenameUtils.removeExtension(file.getName());
+  }
+
+  private int countStopWords() throws IOException {
+    String pathName = "src/main/java/resources/" + language;
+    Path path = Paths.get(pathName);
+    int lines;
+    try (Stream<String> stream = Files.lines(path)) {
+      lines = (int) stream.count();
     }
+    return lines;
+  }
 
-    private void retrieveLanguage() {
-        language = metadata.get("dc.language");
+  private CharArraySet getLanguageStopWords() throws IOException {
+    CharArraySet stopWords = new CharArraySet(countStopWords(), true);
+    try (BufferedReader br = new BufferedReader(
+        new FileReader("src/main/java/resources/stopwords/" + language))) {
+      String line;
+      while ((line = br.readLine()) != null) {
+        stopWords.add(line);
+      }
+    }
+    return stopWords;
+  }
 
-        if (language == null) {
-            language = metadata.get("Content-Language");
-            if (language == null) {
-                language = identifyLanguage();
-            }
+  private Analyzer selectAnalyzer(String mode) throws IOException {
+    switch (mode) {
+      case "language":
+        switch (language) {
+          case "en":
+            return new StopAnalyzer(EnglishAnalyzer.ENGLISH_STOP_WORDS_SET);
+          case "el":
+            return new GreekAnalyzer(getLanguageStopWords());
+          case "fr":
+            return new FrenchAnalyzer(getLanguageStopWords());
+          case "it":
+            return new ItalianAnalyzer(getLanguageStopWords());
         }
+      case "standard":
+        return new StandardAnalyzer();
+      default:
+        return new WhitespaceAnalyzer();
+    }
+  }
+
+  public Map<String, Integer> tokenizeDocument(String mode) throws IOException {
+    Analyzer an = selectAnalyzer(mode);
+    Map<String, Integer> tokens = new HashMap<>();
+    TokenStream stream = an.tokenStream(null, textHandler.toString());
+
+    stream.reset();
+    while (stream.incrementToken()) {
+      String token = stream.getAttribute(CharTermAttribute.class).toString();
+      if (!tokens.containsKey(token)) {
+        tokens.put(token, 1);
+      } else {
+        tokens.replace(token, tokens.get(token) + 1);
+      }
+    }
+    stream.end();
+    stream.close();
+
+    return tokens;
+  }
+
+  public void writeStats() {
+
+  }
+
+  private String createFile(String mode, String fileType) throws IOException {
+    String filename = getFilenameWithoutExtension() + "-" + mode;
+    String filePath = "src/main/resources/" + fileType + "/" + filename + ".dat";
+    File file = new File(filePath);
+
+    if (file.createNewFile()) {
+      System.out.println("File " + filename + " created.");
+    } else {
+      System.out.println("File " + filename + " already exists. "
+          + "Proceeding to delete its contents.");
+      new FileWriter(filePath, false).close();
+      System.out.println("File contents deleted.");
     }
 
-    private int countStopWords() throws IOException {
-        String pathName = "src/main/java/resources/" + language;
-        Path path = Paths.get(pathName);
-        int lines;
-        try (Stream<String> stream = Files.lines(path)) {
-            lines = (int) stream.count();
-        }
-        return lines;
+    return filePath;
+  }
+
+  private void writeDataInFile(String mode, String fileType) throws IOException {
+    List<Entry<String, Integer>> sortedTokens = getSortedTokens(mode);
+    String filePath = createFile(mode, fileType);
+
+    try (FileWriter writer = new FileWriter(filePath)) {
+      for (Map.Entry<String, Integer> token : sortedTokens) {
+        writer.write(token.getKey() + " " + token.getValue()
+            + String.format("%n"));
+      }
+      System.out.println("Tokens saved." + String.format("%n"));
+    } catch (IOException e) {
+      System.out.println("Error: " + e.getMessage());
+    }
+  }
+
+  private List<Entry<String, Integer>> getSortedTokens(String mode) {
+    Map<String, Integer> tokens;
+    String printedString;
+    switch (mode) {
+      case "language":
+        tokens = languageTokens;
+        printedString = "Language";
+      case "standard":
+        tokens = standardTokens;
+        printedString = "Standard";
+      default:
+        tokens = whitespaceTokens;
+        printedString = "Whitespace";
     }
 
-    private CharArraySet getLanguageStopWords() throws IOException {
-        CharArraySet stopWords = new CharArraySet(countStopWords(), true);
-        try (BufferedReader br = new BufferedReader(new FileReader("src/main/java/resources/" + language))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                stopWords.add(line);
-            }
-        }
-        return stopWords;
-    }
+    List<Entry<String, Integer>> sortedTokens = new ArrayList<>(tokens.entrySet());
+    sortedTokens.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+    return sortedTokens;
+  }
 
-    private Analyzer selectAnalyzer(String mode) throws IOException {
-        switch (mode) {
-            case "language":
-                switch (language) {
-                    case "en":
-                        return new StopAnalyzer(EnglishAnalyzer.ENGLISH_STOP_WORDS_SET);
-                    case "el":
-                        return new GreekAnalyzer(getLanguageStopWords());
-                    case "fr":
-                        return new FrenchAnalyzer(getLanguageStopWords());
-                    case "it":
-                        return new ItalianAnalyzer(getLanguageStopWords());
-                }
-            case "standard":
-                return new StandardAnalyzer();
-            default:
-                return new WhitespaceAnalyzer();
-        }
-    }
+  public String getName() {
+    return file.getName();
+  }
 
-    public Map<String, Integer> tokenizeDocument(String mode) throws IOException {
-        Analyzer an = selectAnalyzer(mode);
-        Map<String, Integer> tokens = new HashMap<>();
-        TokenStream stream = an.tokenStream(null, textHandler.toString());
+  public String getLanguage() {
+    return language;
+  }
 
-        stream.reset();
-        while (stream.incrementToken()) {
-            String token = stream.getAttribute(CharTermAttribute.class).toString();
-            if (!tokens.containsKey(token)) {
-                tokens.put(token, 1);
-            } else {
-                tokens.replace(token, tokens.get(token) + 1);
-            }
-        }
-        stream.end();
-        stream.close();
-
-        return tokens;
-    }
-
-
-    public String getName() {
-        return file.getName();
-    }
-
-    public String getLanguage() {
-        return language;
-    }
-
-    public String toString() {
-        return textHandler.toString();
-    }
+  public String toString() {
+    return textHandler.toString();
+  }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,29 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Map;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Ctrl+. with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws Exception {
+        File directory = new File("src/main/resources/books");
+        File[] fileList = directory.listFiles();
+        ArrayList<Document> documentList = new ArrayList<>();
+        assert fileList != null;
 
-        // Press Ctrl+F5 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press F5 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing F9.
-            System.out.println("i = " + i);
+        for (File file : fileList) {
+            Document document = new Document(file);
+            documentList.add(document);
         }
+
+        for (Document document : documentList) {
+            System.out.println(document);
+        }
+
+
+
     }
+
+
 
 }
