d3a26b1b1dd5be73828ab39fb0d69c6ab44cf4fe
DaniCortes <danielcortes2001@gmail.com>
2023-10-30 18:42:02 +0100

    Third commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,25 +5,24 @@ import java.util.ArrayList;
 import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        File directory = new File("src/main/resources/books");
-        File[] fileList = directory.listFiles();
-        ArrayList<Document> documentList = new ArrayList<>();
-        assert fileList != null;
 
-        for (File file : fileList) {
-            Document document = new Document(file);
-            documentList.add(document);
-        }
+  public static void main(String[] args) throws Exception {
+    File directory = new File("src/main/resources/books");
+    File[] fileList = directory.listFiles();
+    ArrayList<Document> documentList = new ArrayList<>();
+    assert fileList != null;
 
-        for (Document document : documentList) {
-            System.out.println(document);
-        }
-
-
-
+    for (File file : fileList) {
+      Document document = new Document(file);
+      documentList.add(document);
     }
 
+    for (Document document : documentList) {
+      System.out.println(document);
+    }
+
+
+  }
 
 
 }
